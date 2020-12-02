abe0f342296a55e3c3143cc689346abe07f5ff3a
Lime5005 <zlp5005@gmail.com>
2020-12-02 02:52:30 +0100

    first commit

---
+++ loveCal.js
@@ -0,0 +1,11 @@
+// var n = Math.random();
+// n = n * 6 + 1
+// n = Math.floor(n); // get number from 1 to 6
+//
+// console.log(n);
+
+//How to get number from 1 to 100 ?
+var m = Math.random();
+m = m * 100 + 1
+m = Math.floor(m);
+console.log(m);
